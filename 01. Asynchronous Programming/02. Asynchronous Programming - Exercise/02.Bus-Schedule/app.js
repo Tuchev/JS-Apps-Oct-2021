cf726b72ff6001091e26b63804be513b130fdd60
Георги Тучев <78377593+Tuchev@users.noreply.github.com>
2021-11-08 12:16:47 +0200

    Add files via upload

---
+++ 01. Asynchronous Programming/02. Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
@@ -0,0 +1,35 @@
+function solve() {
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const infoDiv = document.getElementById('info');
+
+    let currentStopId = 'depot';
+    let stopName = '';
+
+    async function depart() {
+        departBtn.disabled = true;
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${currentStopId}`;
+
+        const res = await fetch(url);
+        const data = await res.json();
+
+        infoDiv.textContent = `Next Stop ${data.name}`;
+        currentStopId = data.next;
+        stopName = data.name;
+        arriveBtn.disabled = false;
+    }
+
+    function arrive() {
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
+
+        infoDiv.textContent = `Arriving at ${stopName}`;
+    }
+
+    return {
+        depart,
+        arrive
+    };
+}
+
+let result = solve();
